141acf89424406c83126ea3ac9960c2d50b7c29e
colbort <461234770@qq.com>
2021-07-29 14:35:05 +0800

    init

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Program_Files\fvm\versions\2.0.5"
+export "FLUTTER_APPLICATION_PATH=E:\work\flutter_test\flutter_install_plugin"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_BUILD_NAME=2.0.1"
+export "FLUTTER_BUILD_NUMBER=2.0.1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
